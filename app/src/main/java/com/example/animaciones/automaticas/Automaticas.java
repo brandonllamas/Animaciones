fde22f80ab88d9252613d2886f47178c504c4e60
Brandon Llamas Larios <brandonllamaslarios@gmail.com>
2021-06-08 21:47:16 -0500

    mm

--- app/src/main/java/com/example/animaciones/automaticas/Automaticas.java
+++ app/src/main/java/com/example/animaciones/automaticas/Automaticas.java
@@ -4,15 +4,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.animaciones.R;
 
 public class Automaticas extends AppCompatActivity {
-
+    Button btnSet;
+    TextView txtSet;
+    Boolean isVisitble= true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_automaticas);
+        btnSet = findViewById(R.id.button);
+        txtSet = findViewById(R.id.textView);
 
+        btnSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isVisitble){
+                    txtSet.setVisibility(View.GONE);
+                }else{
+                    txtSet.setVisibility(View.VISIBLE);
+                }
+                isVisitble =!isVisitble;
+            }
+        });
     }
 }
